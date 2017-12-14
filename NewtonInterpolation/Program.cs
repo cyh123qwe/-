44895c68d441ef998f369d7c567797d74e4e9381
cyh123qwe <33320688+cyh123qwe@users.noreply.github.com>
2017-12-14 21:00:00 +0800

    fix more bugs

--- NewtonInterpolation/Program.cs
+++ NewtonInterpolation/Program.cs
@@ -12,6 +12,7 @@ namespace NewtonInterpolation
         {
             //Console.WriteLine("Hello World!");
             //初始化x
+            /* 
             double[] x=new double[10];
             for(int i =0;i<10;i++)
             {
@@ -21,11 +22,17 @@ namespace NewtonInterpolation
             List<List<double>> table= Newton.TableCal(f,x,false);
             foreach(var i in table)
             Console.WriteLine(i);
+            */
+            
+            double[] x=new double[4]{-1,0,1,3};
+            double[] y=new double[4]{1,-1,-1,29};
+            List<List<double>> table=Newton.TableCal(y,x,false);
+            Console.ReadKey();
             
         }
         static double f(double x)
         {
-            return -Pow(x,8)+5*Pow(x,7)-3*x-1;
+            return Pow(x,8)+5*Pow(x,7)-3*x-1;
         }
     }
     public static class Newton
@@ -41,9 +48,16 @@ namespace NewtonInterpolation
             List<double> y= x.ToList();
             y=y.Select(e=>e=f(e)).ToList();//计算初始值
             if(d)
-            return InnerCal(y,x.ToList(),diff);
+            return InnerCal(y,x.ToList(),diff);//差分情况
             else
-            return InnerCal(y,x.ToList(),difq);
+            return InnerCal(y,x.ToList(),difq);//差商情况
+        }
+        public static List<List<double>> TableCal(double[] y,double[] x,bool d=true)
+        {
+            if(d)
+            return InnerCal(y.ToList(),x.ToList(),diff);//差分情况
+            else
+            return InnerCal(y.ToList(),x.ToList(),difq);//差商情况
         }
         //计算各阶差商，两个list嵌套封装成一个类会好一点
         private static List<List<double>> InnerCal(List<double> y,List<double> x,Func<int,int,List<double>,List<double>,double> dif)
@@ -72,7 +86,7 @@ namespace NewtonInterpolation
         //用于差商的函数
         public static double difq(int i,int j,List<double> Last,List<double> x)
         {
-            return (Last[i]-Last[i-1])/(x[i+j-1]-x[i-i]);
+            return (Last[i]-Last[i-1])/(x[i+j-1]-x[i-1]);
         }
         //用于差分
         public static double diff(int i,int j,List<double> Last,List<double> x)
