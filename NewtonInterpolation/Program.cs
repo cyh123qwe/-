45b1372616bf80479f49fd2484adadc864c1207b
cyh123qwe <33320688+cyh123qwe@users.noreply.github.com>
2017-12-14 20:34:36 +0800

    fix some bugs

--- NewtonInterpolation/Program.cs
+++ NewtonInterpolation/Program.cs
@@ -11,16 +11,21 @@ namespace NewtonInterpolation
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
+            //初始化x
             double[] x=new double[10];
             for(int i =0;i<10;i++)
             {
                 x[i]=1.0/(i+1);
             }
-            List<List<double>> table= Newton.TableCal(f,x);
+            //计算差商
+            List<List<double>> table= Newton.TableCal(f,x,false);
+            foreach(var i in table)
+            Console.WriteLine(i);
+            
         }
         static double f(double x)
         {
-            return Pow(x,8)+5*Pow(x,7)-3*x-1;
+            return -Pow(x,8)+5*Pow(x,7)-3*x-1;
         }
     }
     public static class Newton
@@ -30,15 +35,18 @@ namespace NewtonInterpolation
             return 0;
         }
         //计算差商表的函数
-        public static List<List<double>> TableCal(Func<double,double> f,double[] x)
+        //在默认情况下计算差分表。d参数输入false计算差商表
+        public static List<List<double>> TableCal(Func<double,double> f,double[] x,bool d=true)
         {
             List<double> y= x.ToList();
             y=y.Select(e=>e=f(e)).ToList();//计算初始值
-
-            return InnerCal(y);
+            if(d)
+            return InnerCal(y,x.ToList(),diff);
+            else
+            return InnerCal(y,x.ToList(),difq);
         }
-        //计算各阶差分，两个list嵌套封装成一个类会好一点
-        private static List<List<double>> InnerCal(List<double> y)
+        //计算各阶差商，两个list嵌套封装成一个类会好一点
+        private static List<List<double>> InnerCal(List<double> y,List<double> x,Func<int,int,List<double>,List<double>,double> dif)
         {
             List<List<double>> result=new List<List<double>>();
             result.Add(y);
@@ -49,12 +57,27 @@ namespace NewtonInterpolation
                 List<double> temp =new List<double>();
                 for(int i =1;i<result.Last().Count();i++)
                 {
-                    double t=result.Last()[i]-result.Last()[i-1];
+                    //计算差分
+                    //double t=result.Last()[i]-result.Last()[i-1];
+                    //计算差商
+                    int j=result.Count();
+                    double t=dif(i,j,result.Last(),x);
+                    //double t=(result.Last()[i]-result.Last()[i-1])/(x[i]-x[i-(j-1)]);
                     temp.Add(t);
                 }
                 result.Add(temp);
             }
             return result;
+        }
+        //用于差商的函数
+        public static double difq(int i,int j,List<double> Last,List<double> x)
+        {
+            return (Last[i]-Last[i-1])/(x[i+j-1]-x[i-i]);
+        }
+        //用于差分
+        public static double diff(int i,int j,List<double> Last,List<double> x)
+        {
+            return Last[i]-Last[i-1];
         }
     }
 }
