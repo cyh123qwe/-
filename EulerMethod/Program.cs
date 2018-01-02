7a44c8dcad2ba246cbffc7f4d1f4858c6f37ee15
cyh123qwe <33320688+cyh123qwe@users.noreply.github.com>
2018-01-02 10:50:22 +0800

    添加欧拉积分

---
+++ EulerMethod/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Collections.Generic;
+using static System.Math;
+using static System.Console;
+using static EulerMethod.EulerMethod;
+namespace EulerMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Hello World!");
+           Dictionary<double,double> result=EulerCal(testfunc,0,1,10,1);
+           foreach(var i in result)
+           {
+               WriteLine("x="+i.Key+"   "+"y="+i.Value);
+           }
+        }
+        public static double testfunc(double x, double y)
+        {
+            return y-2*x/y;
+        } 
+    }
+    public static class EulerMethod
+    {
+        public static Dictionary<double,double> EulerCal(Func<double,double,double> f,double a,double b,int n,double y0)
+        {
+            Dictionary<double,double> result =new Dictionary<double,double>();
+            double y=y0;
+            double x=a;
+            result.Add(x,y);
+            double h=(b-a)/n;
+            for(int i=0;i<n;i++)
+            {
+                y=y+h*f(x,y);
+                x=x+h;
+                result.Add(x,y);
+                
+            }
+            return result;
+        }
+    }
+}
---
+++ calculas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using System.Linq;
+using static calculas.Calculator;
+using static System.Console;
+using static System.Math;
+namespace calculas
+{
+    //复化求积公式编程
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            WriteLine("复化梯形公式结果="+trapezoid(x=>Exp(-x),0,1,41));
+            WriteLine("复化Simpson公式结果="+simpson(x=>Exp(-x),0,1,4));
+            Read();
+        }
+    }
+    public static class Calculator
+    {
+        public static double trapezoid(Func<double,double> f ,double a,double b,int n)
+        {
+            double h=(b-a)/n;
+            double result = 0;
+            for(int i =0;i<=n;i++)
+            {
+                result = result + (f(a+i*h)+f(a+(i+1)*h))*h/2;   
+            }
+            return result;
+        }
+        public static double simpson(Func<double,double> f,double a ,double b,int n)
+        {
+            double h=(b-a)/n;
+            int m =n/2;
+            double result=0;
+            for(int k=1;k<=m;k++)
+            {
+                result=result + (
+                                f(a+(2*k-2)*h)
+                                +4*f(a+(2*k-1)*h)
+                                +f(a+(2*k)*h))*h/3;
+            }
+            return result ;
+        }
+    }
+
+}
